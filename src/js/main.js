a2905bc2d1a0c718969de8554f4f3a8e4e454711
Goran Jovanovic <goranj@vast.com>
2018-07-30 21:28:06 +0200

    init

---
+++ src/js/main.js
@@ -0,0 +1,37 @@
+var socket = io.connect();
+socket.on('connect', function(data) {
+    socket.emit('join', 'Hello World from client');
+});
+socket.on('broad', function(data) {
+  $('#future').append(`<li class="list-group-item">${data}</li>`);
+  if ($("#future li").length > 5) {
+    $('#future li').first().remove();
+  }
+
+});
+
+$('form').submit(function(e){
+    e.preventDefault();
+    var message = $('#chat_input').val();
+    socket.emit('messages', message);
+});
+
+var demoColorPicker = new iro.ColorPicker("#color-picker-container", {
+  width: 320,
+  height: 320,
+  color: {r: 255, g: 0, b: 0},
+  markerRadius: 8,
+  padding: 4,
+  sliderMargin: 24,
+  sliderHeight: 36,
+  borderWidth: 2,
+  borderColor: "#ccc",
+  anticlockwise: true,
+});
+demoColorPicker.on("color:change", function(color, changes) {
+  $('#future').css('color', color.hexString);
+
+  // Log the color's hex RGB value to the dev console
+  //console.log(color.rgb);
+  socket.emit('color', color.rgb);
+});
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: './src/js/app.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        loaders: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(svg|gif|png|eot|woff|ttf)$/,
+        loaders: [
+          'url-loader'
+        ]
+      }
+    ]
+  }
+}

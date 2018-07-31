fa6594df690b24e06c738040e04df24b85cd6f27
azzazelo <azzazel@unseen.is>
2018-07-31 21:39:00 +0200

    babel, eslint

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,7 @@
 
 import 'bulma/css/bulma.min.css';
-
 import '../css/main.css';
-
 window.jQuery = window.$ = require('jquery');
 window.io = require('socket.io-client/dist/socket.io.js');
 window.iro = require('@jaames/iro/dist/iro.js');
-
 require('./main.js');
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,22 @@ module.exports = {
         loaders: [
           'url-loader'
         ]
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["babel-preset-env"]
+          }
+        }
       }
     ]
   },

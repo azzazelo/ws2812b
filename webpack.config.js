80fa4fec0aff1452082cfcb4e8712260312a4359
azzazel <azzazel@gmail.com>
2018-08-02 01:26:28 +0200

    ,

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
         enforce: 'pre',
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'eslint-loader',
+        loader: 'eslint-loader'
       },
       {
         test: /\.js$/,
